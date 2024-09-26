2edd1848167eb005eb49a2ccb0d8d4133439b2a6
WeulerSilva <weulergamer@outlook.com>
2024-09-25 22:08:28 -0300

    change de hight og the button on the carrosel

--- src/app/components/FirstHomeCarrosel.tsx
+++ src/app/components/FirstHomeCarrosel.tsx
@@ -34,38 +34,38 @@ export const FirstHomeCarrosel = () => {
                          ${changeBanner === 9 ? "bg-banner-home-10-m md:bg-banner-home-10" : ""}
                          `}>
 
-                        <div className='w-[300px] h-[30px] pb-4 md:h-[10px] lg:h-[30px]'>
+                        <div className='w-[300px] h-[30px] pb-4 md:h-[10px] lg:h-[10px]'>
                             <div className="w-full h-full flex justify-between items-center">
-                                <div className={`w-[15px] h-[15px] ${changeBanner === 0 ? 'bg-laranja' : 'bg-white'} rounded-full
+                                <div className={`w-[10px] h-[10px] ${changeBanner === 0 ? 'bg-laranja' : 'bg-white'} rounded-full
                                 hover:bg-laranja border-[1px] border-black`}
                                     onClick={() => setChangeBanner(0)}></div>
 
-                                <div className={`w-[15px] h-[15px] ${changeBanner === 1 ? 'bg-laranja' : 'bg-white'} rounded-full
+                                <div className={`w-[10px] h-[10px] ${changeBanner === 1 ? 'bg-laranja' : 'bg-white'} rounded-full
                                 hover:bg-laranja border-[1px] border-black`}
                                     onClick={() => setChangeBanner(1)}></div>
 
-                                <div className={`w-[15px] h-[15px] ${changeBanner === 2 ? 'bg-laranja' : 'bg-white'} rounded-full
+                                <div className={`w-[10px] h-[10px] ${changeBanner === 2 ? 'bg-laranja' : 'bg-white'} rounded-full
                                 hover:bg-laranja border-[1px] border-black`}
                                     onClick={() => setChangeBanner(2)}></div>
-                                <div className={`w-[15px] h-[15px] ${changeBanner === 3 ? 'bg-laranja' : 'bg-white'} rounded-full
+                                <div className={`w-[10px] h-[10px] ${changeBanner === 3 ? 'bg-laranja' : 'bg-white'} rounded-full
                                 hover:bg-laranja border-[1px] border-black`}
                                     onClick={() => setChangeBanner(3)}></div>
-                                <div className={`w-[15px] h-[15px] ${changeBanner === 4 ? 'bg-laranja' : 'bg-white'} rounded-full
+                                <div className={`w-[10px] h-[10px] ${changeBanner === 4 ? 'bg-laranja' : 'bg-white'} rounded-full
                                 hover:bg-laranja border-[1px] border-black`}
                                     onClick={() => setChangeBanner(4)}></div>
-                                <div className={`w-[15px] h-[15px] ${changeBanner === 5 ? 'bg-laranja' : 'bg-white'} rounded-full
+                                <div className={`w-[10px] h-[10px] ${changeBanner === 5 ? 'bg-laranja' : 'bg-white'} rounded-full
                                 hover:bg-laranja border-[1px] border-black`}
                                     onClick={() => setChangeBanner(5)}></div>
-                                <div className={`w-[15px] h-[15px] ${changeBanner === 6 ? 'bg-laranja' : 'bg-white'} rounded-full
+                                <div className={`w-[10px] h-[10px] ${changeBanner === 6 ? 'bg-laranja' : 'bg-white'} rounded-full
                                 hover:bg-laranja border-[1px] border-black`}
                                     onClick={() => setChangeBanner(6)}></div>
-                                <div className={`w-[15px] h-[15px] ${changeBanner === 7 ? 'bg-laranja' : 'bg-white'} rounded-full
+                                <div className={`w-[10px] h-[10px] ${changeBanner === 7 ? 'bg-laranja' : 'bg-white'} rounded-full
                                 hover:bg-laranja border-[1px] border-black`}
                                     onClick={() => setChangeBanner(7)}></div>
-                                    <div className={`w-[15px] h-[15px] ${changeBanner === 8 ? 'bg-laranja' : 'bg-white'} rounded-full
+                                    <div className={`w-[10px] h-[10px] ${changeBanner === 8 ? 'bg-laranja' : 'bg-white'} rounded-full
                                 hover:bg-laranja border-[1px] border-black`}
                                     onClick={() => setChangeBanner(8)}></div>
-                                    <div className={`w-[15px] h-[15px] ${changeBanner === 9 ? 'bg-laranja' : 'bg-white'} rounded-full
+                                    <div className={`w-[10px] h-[10px] ${changeBanner === 9 ? 'bg-laranja' : 'bg-white'} rounded-full
                                 hover:bg-laranja border-[1px] border-black`}
                                     onClick={() => setChangeBanner(9)}></div>
                             </div>
